023e533404cf07b76b857f4cc7c58271abcf79e6
egheCodes <ehioroboeghe@gmail.com>
2020-01-07 14:41:52 +0100

    fixed bugs

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.put('/image/',  (req, res) => {image.handleImage(req, res, db)} );
 
 app.post('/imageurl/',  (req, res) => {image.handleApiCall(req, res)} );
 
-app.listen(process.env.PORT || 3002, () => {
+app.listen(process.env.PORT, () => {
 	console.log(`app is running on port ${process.env.PORT}`)
 });
 
